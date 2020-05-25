4c198df848bb8bf1190244aa5ef38829614101ab
Mansi Chauhan <mansichauhan15@gmail.com>
2020-05-25 16:33:14 +0530

    Initial Commit

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  calc:any = "test";
+  clr(){
+
+    this.calc="";
+
+  }
+
+  val: any;
+  display(val){
+    console.log(val);
+    this.calc += val;
+  }
+
+  res:any;
+  solve(){
+
+    this.res = eval(this.calc);
+    this.calc = this.res;
+
+  }
+}
